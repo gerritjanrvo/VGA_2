234b46a19c882287719175ca071974bd8619fd6d
JOOST-KOUIJZER\Joost <joostkouijzer@gmail.com>
2018-05-23 17:06:10 +0200

    Werkende input kan naar teken functies schrijven

--- logic_Layer.c
+++ logic_Layer.c
@@ -15,6 +15,7 @@ struct dataStruct
 	char data7[20];
 	char data8[20];
 };
+
 struct dataStruct dataArg;
 char *p = (char*)&dataArg;
 
@@ -49,45 +50,45 @@ void handleArgument(int nr, char *arg)
 			case 1:
 				UART_puts("\n Twee=");
 				//UART_puts(arg);
-				strcpy (dataArg.data1, arg);
-				UART_puts(dataArg.data1);
+				strcpy (dataArg.data2, arg);
+				UART_puts(dataArg.data2);
 				break;
 
 			case 2:
 				UART_puts("\n Drie=");
 				//UART_puts(arg);
-				strcpy (dataArg.data1, arg);
-				UART_puts(dataArg.data1);
+				strcpy (dataArg.data3, arg);
+				UART_puts(dataArg.data3);
 				break;
 			case 3:
 				UART_puts("\n Vier=");
 				//UART_puts(arg);
-				strcpy (dataArg.data1, arg);
-				UART_puts(dataArg.data1);
+				strcpy (dataArg.data4, arg);
+				UART_puts(dataArg.data4);
 				break;
 			case 4:
 				UART_puts("\n Vijf=");
 				//UART_puts(arg);
-				strcpy (dataArg.data1, arg);
-				UART_puts(dataArg.data1);
+				strcpy (dataArg.data5, arg);
+				UART_puts(dataArg.data5);
 				break;
 			case 5:
 				UART_puts("\n Zes=");
 				//UART_puts(arg);
-				strcpy (dataArg.data1, arg);
-				UART_puts(dataArg.data1);
+				strcpy (dataArg.data6, arg);
+				UART_puts(dataArg.data6);
 				break;
 			case 6:
 				UART_puts("\n Zeven=");
 				//UART_puts(arg);
-				strcpy (dataArg.data1, arg);
-				UART_puts(dataArg.data1);
+				strcpy (dataArg.data7, arg);
+				UART_puts(dataArg.data7);
 				break;
 			case 7:
 				UART_puts("\n Acht=");
 				//UART_puts(arg);
-				strcpy (dataArg.data1, arg);
-				UART_puts(dataArg.data1);
+				strcpy (dataArg.data8, arg);
+				UART_puts(dataArg.data8);
 				break;
 			default:
 				UART_puts("\n Te veel argumenten:");
@@ -97,20 +98,31 @@ void handleArgument(int nr, char *arg)
  }
 
 void handleFirstArgument(){
+	UART_puts("\n in handleFirstArgument: ");
+	UART_puts(dataArg.data1);
 	if(strcmp(dataArg.data1, "lijn")== 0) { UART_puts("\n Hij heeft lijn");
-		 //tekenLijn(een,twee,drie,vier,vijf,zes);
+		maak_lijn ();
 	}
 	else if(strcmp(dataArg.data1, "ellips")== 0){UART_puts("\n Hij heeft ellips");
+		maak_ellips();
 	}
 	if(strcmp(dataArg.data1, "rechthoek")== 0) { UART_puts("\n Hij heeft rechthoek");
+		maak_rechthoek();
 	}
 	else if(strcmp(dataArg.data1, "driehoek")== 0){ UART_puts("\n Hij heeft driehoek");
+		maak_driehoek();
 	}
 	if(strcmp(dataArg.data1, "tekst")== 0) { UART_puts("\n Hij heeft tekst");
+		maak_tekst();
 	}
 	else if(strcmp(dataArg.data1, "bitmap")== 0){ UART_puts("\n Hij heeft bitmap");
+		maak_bitmap();
 	}
-	if(strcmp(dataArg.data1, "wacht")== 0) { UART_puts("\n Hij heeft wacht");
+	if(strcmp(dataArg.data1, "clearscherm")== 0) { UART_puts("\n Hij heeft clearscherm");
+		maak_clearscherm();
+	}
+	else if(strcmp(dataArg.data1, "wacht")== 0) { UART_puts("\n Hij heeft wacht");
+		wacht();
 	}
 }
 
